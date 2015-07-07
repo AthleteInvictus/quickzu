57305002dc3e8a98e350be2e2063891addf0bf52
AthleteInvictus <cantucade@gmail.com>
2015-07-06 22:41:14 -0400

    Updated

--- app/src/main/java/com/classycritic/quickzu/MainActivity.java
+++ app/src/main/java/com/classycritic/quickzu/MainActivity.java
@@ -1,10 +1,12 @@
 package com.classycritic.quickzu;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -40,7 +42,6 @@ public class MainActivity extends Activity {
             super.onBackPressed();
         }
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
